a3af8e781783658497370d7efb1593c1ce2e9ed0
Holger Maerz <holger@nakade.de>
2019-07-17 22:34:34 +0200

    updates in composer

--- src/Controller/Common.php
+++ src/Controller/Common.php
@@ -4,7 +4,20 @@
 namespace App\Controller;
 
 
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class Common
+ * @package App\Controller
+ */
 class Common
 {
+    public function number()
+    {
+        $number = random_int(0, 100);
 
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
 }
