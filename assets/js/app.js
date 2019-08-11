986f450f7af73f2e7a71c9e0d20c2f706f97e203
Holger Maerz <holger@nakade.de>
2019-08-11 15:13:36 +0200

    twig templates

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,10 +6,9 @@
  */
 
 // any CSS you require will output into a single css file (app.css in this case)
-require('../css/main.css');
-
-const imagesContext = require.context('../images', true, /\.(png|jpg|jpeg|gif|ico|svg|webp)$/);
-imagesContext.keys().forEach(imagesContext);
+require('../css/app.css');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // const $ = require('jquery');
+
+console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
