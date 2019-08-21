970a2b033091716d6b78116e8bd2348ee64137f3
Holger Maerz <holger@nakade.de>
2019-08-21 13:07:49 +0200

    bootstrap imported by encore ; edited base.html ; two nav bars , a sticky note and a version tag

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,12 +8,11 @@
 
 // any CSS you require will output into a single css file (app.css in this case)
 import '../css/app.scss';
-import $ from 'jquery';
-import 'bootstrap'; //add functions to jQuery
-//uncomment to support legacy code
-//global.$ = $;
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// const $ = require('jquery');
+const $ = require('jquery');
+import 'bootstrap'; //add functions to jQuery
+//uncomment to support legacy code
+global.$ = $;
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
