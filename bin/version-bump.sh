3aa78078b1b03ebd9f9cf2dd816991f865a70288
Holger Maerz <holger@nakade.de>
2019-08-26 22:04:54 +0200

    version properties

---
+++ bin/version-bump.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# bash script for automatic version setting in build.xml
+# EXPECTING TWO PARAMS: FIRST ROOT DIR, 2nd VERSION
+BASEDIR=$1
+VERSION=$2
+
+TEMPLATE=base.html.twig
+PATH="$BASEDIR"/templates/"$TEMPLATE"
+
+echo Base directory is "$BASEDIR"
+
+#invalid version
+if [ "$#" -eq 0 ]; then
+    echo "No valid version supplied. Exit."
+    exit 1
+fi
+
+echo New version is $1
+
+#creating file version.properties
+sed -i 's/project.version = .*$/project.version = '$VERSION'/' $BASEDIR/version.properties 2>/dev/null
+if [ $? != 0 ]; then
+    echo project.version = $VERSION >> $BASEDIR/project.properties
+fi
+
+
+#template file existing
+if [ ! -f "$PATH" ]
+then
+        echo File "$PATH" not found.
+        echo Exit.
+        exit 1
+else    echo File "$PATH" found.
+fi
+
+#replacing version in file
+sed -i 's/<li class="version">.*</<li class="version">'$VERSION'</' "$PATH" 2>/dev/null
+if [ $? -ne 0 ]
+then
+    echo Could not edit template
+    exit 1
+else echo Set new version in template.
+fi
