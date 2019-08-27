afeec0e74170db17b330a11fbd83f24cb1bd735a
Holger Maerz <holger@nakade.de>
2019-08-27 12:04:58 +0200

    version properties#12

--- version-bump.sh
+++ version-bump.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # bash script for automatic version setting in build.xml
-# EXPECTING TWO PARAMS: FIRST ROOT DIR, 2nd VERSION
-BASEDIR=$1
-VERSION=$2
+# EXPECTING VERSION AS PARAMETER
+VERSION=$1
 
-TEMPLATE=base.html.twig
-PATH="$BASEDIR"/templates/"$TEMPLATE"
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+TEMPLATE="footer.html.twig"
+TMP_PATH="$BASEDIR"/templates/"$TEMPLATE"
 
 echo Base directory is "$BASEDIR"
 
@@ -17,27 +17,26 @@ fi
 
 echo New version is $1
 
-#creating file version.properties
-sed -i 's/project.version = .*$/project.version = '$VERSION'/' $BASEDIR/version.properties 2>/dev/null
+#creating file project.properties
+sed -i 's/project.version = .*$/project.version = '$VERSION'/' $BASEDIR/project.properties 2>/dev/null
 if [ $? != 0 ]; then
-    echo project.version = $VERSION >> $BASEDIR/project.properties
+  echo project.version = $VERSION >> $BASEDIR/project.properties
 fi
 
-
 #template file existing
-if [ ! -f "$PATH" ]
+if [ ! -f "$TMP_PATH" ]
 then
-        echo File "$PATH" not found.
+        echo File "$TMP_PATH" not found.
         echo Exit.
         exit 1
-else    echo File "$PATH" found.
+else    echo File "$TMP_PATH" found.
 fi
 
 #replacing version in file
-sed -i 's/<li class="version">.*</<li class="version">'$VERSION'</' "$PATH" 2>/dev/null
+sed -i 's/ class="version">.*</ class="version">'$VERSION'</' "$TMP_PATH" 2>/dev/null
 if [ $? -ne 0 ]
 then
-    echo Could not edit "$PATH"
+    echo Could not edit "$TMP_PATH"
     exit 1
 else echo Set new version in template.
 fi
