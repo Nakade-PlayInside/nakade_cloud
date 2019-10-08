6be060b640f3f86aa28848e35ec48a6a96e7ef13
Holger Maerz <holger@nakade.de>
2019-10-08 17:36:34 +0200

    profile scripting

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,3 +1,11 @@
 import '../css/profile.scss';
 
 //navigation active class settings
+$("#news-toggle").change(function () {
+
+    if (this.checked === true) {
+        $.ajax({ url: 'script.php?argument=value&foo=bar' });
+    } else {
+        alert("aus");
+    }
+});
