029643924256b314244f396cdb754894674af36d
Holger Maerz <holger@nakade.de>
2019-12-03 17:33:47 +0100

    mapped super class for mails; timestampable annotations for create and update with default value; migration

--- src/Entity/Bundesliga/LineupMail.php
+++ src/Entity/Bundesliga/LineupMail.php
@@ -7,60 +7,8 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ORM\Entity(repositoryClass="App\Repository\Bundesliga\LineupMailRepository")
  */
-class LineupMail
+class LineupMail extends AbstractMail
 {
-    /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
-     */
-    private $id;
-
-    /**
-     * @ORM\OneToOne(targetEntity="App\Entity\Bundesliga\BundesligaResults", inversedBy="lineupMail", cascade={"persist", "remove"})
-     * @ORM\JoinColumn(nullable=false)
-     */
-    private $results;
-
-    /**
-     * @ORM\Column(type="datetime", nullable=true)
-     */
-    private $sendAt;
-
-    public function __construct(BundesligaResults $results)
-    {
-        $this->results = $results;
-    }
-
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    public function getResults(): ?BundesligaResults
-    {
-        return $this->results;
-    }
-
-    public function setResults(?BundesligaResults $results): self
-    {
-        $this->results = $results;
-
-        return $this;
-    }
-
-    public function getSendAt(): ?\DateTimeInterface
-    {
-        return $this->sendAt;
-    }
-
-    public function setSendAt(?\DateTimeInterface $sendAt): self
-    {
-        $this->sendAt = $sendAt;
-
-        return $this;
-    }
-
     public function getOpponentTeam(): BundesligaTeam
     {
         return $this->results->getOpponentTeam();
@@ -75,15 +23,5 @@ class LineupMail
         $names = explode(' ', $nameOppCaptain);
 
         return array_shift($names);
-    }
-
-    public function getLeagueNumber(): int
-    {
-        return intval($this->results->getSeason()->getLeague());
-    }
-
-    public function getLeagueGroup(): string
-    {
-        return preg_replace('/[0-9]+/', '', $this->results->getSeason()->getLeague());
     }
 }
